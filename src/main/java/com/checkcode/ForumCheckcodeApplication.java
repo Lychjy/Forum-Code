798cfd9690451385d92e6f3af8d6e7937960cb6d
luoyi <1492698348@qq.com>
2023-07-21 18:59:31 +0800

    Initial commit

---
+++ src/main/java/com/checkcode/ForumCheckcodeApplication.java
@@ -0,0 +1,13 @@
+package com.forumcheckcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumCheckcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumCheckcodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/forumcheckcode/ForumCheckcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forumcheckcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumCheckcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
